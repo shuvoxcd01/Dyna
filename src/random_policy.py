c7da27b01c8a7cb3187859d95eae999e19faabda
shuvoxcd01 <dasshuvo7@gmail.com>
2023-12-12 20:14:15 +0600

    Update policy implementation

--- src/random_policy.py
+++ src/random_policy.py
@@ -1,4 +1,4 @@
-from src.gridworld_mdp import GridWorld
+from src.envs.gridworld_mdp import GridWorld
 
 
 class EquiprobableRandomPolicy:
@@ -9,19 +9,10 @@ class EquiprobableRandomPolicy:
         assert state in self.world_model.states
         assert selected_action in self.world_model.actions
 
-        num_all_possible_actions = 0
-        times_selected_action_chosen = 0
-
-        for next_state in self.world_model.states:
-            for action in self.world_model.actions:
-                if self.world_model.reward_fn(state, action, next_state) == -1:
-                    num_all_possible_actions += 1
-                    if action == selected_action:
-                        times_selected_action_chosen += 1
-
-        if not num_all_possible_actions:
-            return 0
-
-        prob = times_selected_action_chosen / num_all_possible_actions
+       
+        prob = 1.0 / float(len(self.world_model.actions))
 
         return prob
+    
+    def get_action(self, state):
+        return random.choice(list(self.world_model.actions))
