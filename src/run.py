11ce7b3109054ef255b981479c86787b02aff60b
shuvoxcd01 <dasshuvo7@gmail.com>
2023-12-12 20:18:00 +0600

    Change environment.

--- src/run.py
+++ src/run.py
@@ -1,12 +1,14 @@
-from src.gridworld_mdp import GridWorld
+from src.envs.gridworld_mdp import GridWorld
 from src.iterative_policy_evaluation import IterativePolicyEvaluation
 from src.random_policy import EquiprobableRandomPolicy
 
 mdp = GridWorld()
+mdp.reset()
+mdp._render('human')
 policy = EquiprobableRandomPolicy()
 policy_evaluation = IterativePolicyEvaluation(policy=policy, mdp=mdp)
 
-policy_evaluation.estimate_state_value_function_inplace(theta=0.00000000001)
+policy_evaluation.estimate_state_value_function(theta=0.000000000000001)
 
 value_fn = policy_evaluation.get_value_fn()
 
