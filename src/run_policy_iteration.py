9a6080d862a70e78a3313d938c377cd815b4c3ea
shuvoxcd01 <dasshuvo7@gmail.com>
2023-12-20 07:15:09 +0600

    Add policy iteration

---
+++ src/run_policy_iteration.py
@@ -0,0 +1,12 @@
+from src.envs.gridworld_mdp import GridWorld
+from src.policy.random_policy import EquiprobableRandomPolicy
+from src.policy_evaluation.exact_policy_evaluation import ExactPolicyEvaluation
+from src.policy_iteration.policy_iteration import PolicyIteration
+
+
+mdp = GridWorld()
+policy_evaluator = ExactPolicyEvaluation(mdp=mdp)
+initial_policy = EquiprobableRandomPolicy(mdp=mdp)
+
+ob = PolicyIteration(mdp=mdp, policy_evaluator=policy_evaluator)
+print(ob.do_policy_iteration(initial_policy=initial_policy))
