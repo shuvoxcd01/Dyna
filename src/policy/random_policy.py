81a5f588a46b29fcd6c5dfa8182701333dbeb1a8
shuvoxcd01 <dasshuvo7@gmail.com>
2023-12-20 07:17:40 +0600

    Refactor

--- src/policy/random_policy.py
+++ src/policy/random_policy.py
@@ -1,18 +1,19 @@
+import random
 from src.envs.gridworld_mdp import GridWorld
+from src.policy.base_policy import BasePolicy
 
 
-class EquiprobableRandomPolicy:
-    def __init__(self):
-        self.world_model = GridWorld()
+class EquiprobableRandomPolicy(BasePolicy):
+    def __init__(self, mdp):
+        self.world_model = mdp
 
     def get_prob(self, selected_action, state):
         assert state in self.world_model.states
         assert selected_action in self.world_model.actions
 
-       
         prob = 1.0 / float(len(self.world_model.actions))
 
         return prob
-    
+
     def get_action(self, state):
         return random.choice(list(self.world_model.actions))
